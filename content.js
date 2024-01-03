7d2de6b480c3b2ce8414c96445ba65d1f64780de
Joyal George <joyalgeorgepulk@gmail.com>
2024-01-03 22:08:34 +0530

    added random value selector for select boxes

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 function generateFakeName() {
     const names = ["John Doe", "Jane Doe", "Alice", "Bob"];
     return names[Math.floor(Math.random() * names.length)];
+    // return faker.name.findName();
 }
 
 function generateFakeEmail() {
@@ -11,19 +12,25 @@ function generateFakeEmail() {
 function fillInputFields() {
     // In your content script
     // change to only select input fields which are not disabled
-    const inputElements = document.querySelectorAll('input')
+    const inputElements = document.querySelectorAll( 'select')
     console.log("Input fields are =>");
     console.log(inputElements);
     console.log("Displaying each fields");
     inputElements.forEach((input) => {
         console.log(input);
         const inputType = input.type.toLowerCase();
+        console.log("Input type = "+inputType);
         switch (inputType) {
             case "text":
                 input.value=generateFakeName()
                 break;
             case "email":
                 input.value=generateFakeEmail()
+                break;
+            case "select-one":
+                const options = input.getElementsByTagName('option');
+                const randomIndex = Math.floor(Math.random() * options.length);
+                options[randomIndex].selected = true;
                 break;
             default:
                 input.value="add dummy data"
